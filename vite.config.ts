f670fc49ba06d632566662a7b29ead4a2b8985f0
Tafadzwa Manzarame <tafamanza11@gmail.com>
2025-09-18 14:22:08 +0200

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-    strictPort: true,
-    hmr: {
-      clientPort: 443,
-    },
-  },
+    exclude: ['lucide-react']
+  }
 });
